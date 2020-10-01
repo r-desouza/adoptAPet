546bcd82e9a994759b2d8cce7a595cdf9c85b18c
Rafa <rdesouza010@alumnos.iua.edu.ar>
2020-10-01 13:53:13 -0300

    Added a Search Bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+//import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="someting-important">Adopt me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Bulldog" />
-      <Pet name="Tito" animal="Dog" breed="Caniche" />
-      <Pet name="Piggy" animal="Dog" breed="Shitzu" />
+      <SearchParams />
     </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));

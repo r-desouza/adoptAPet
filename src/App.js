0c547f5c7885b23b19edae08f7daa922759a5766
Rafa <rdesouza010@alumnos.iua.edu.ar>
2020-10-01 04:34:08 -0300

    Started using JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Bulldog",
-    }),
-    React.createElement(Pet, {
-      name: "Tito",
-      animal: "Dog",
-      breed: "Caniche",
-    }),
-    React.createElement(Pet, {
-      name: "Piggy",
-      animal: "Dog",
-      breed: "Shitzu",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="someting-important">Adopt me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Bulldog" />
+      <Pet name="Tito" animal="Dog" breed="Caniche" />
+      <Pet name="Piggy" animal="Dog" breed="Shitzu" />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
